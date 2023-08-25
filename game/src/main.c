c2745ecec47ddabcf20519c931dc35740d7d5027
kacken <142664345+kacke8320@users.noreply.github.com>
2023-08-25 10:42:15 +0200

    Add files via upload

--- game/src/main.c
+++ game/src/main.c
@@ -1,6 +1,85 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include "./constants.h"
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int game_is_running = FALSE;
+
+unsigned int initialize_window(void) {
+				if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+								fprintf(stderr, "Error initializing SDL.\n");
+								return FALSE;
+				}
+								 //parameter(title, x, y, height, width, );
+				window = SDL_CreateWindow(
+												NULL,
+												SDL_WINDOWPOS_CENTERED,
+												SDL_WINDOWPOS_CENTERED,
+												WINDOW_WIDTH,
+												WINDOW_HEIGHT,
+												SDL_WINDOW_BORDERLESS
+								 );
+				if(!window) {
+								fprintf(stderr, "Error creating SDL Window.\n");
+								return FALSE;
+				}																//-1 means default, 0 does too
+				renderer = SDL_CreateRenderer(window, -1, 0);
+				if(!renderer) {
+								fprintf(stderr, "Error creating SDL Renderer.\n");
+								return FALSE;
+				}
+				
+				return TRUE;
+}
+
+void process_input() {
+				SDL_Event event;
+				SDL_PollEvent(&event);
+
+				switch(event.type) {
+								case SDL_QUIT:
+												game_is_running = FALSE;
+												break;
+								case SDL_KEYDOWN:
+												if(event.key.keysym.sym == SDLK_ESCAPE)
+																game_is_running = FALSE;
+												break;
+				}
+}
+
+void setup() {
+				//TODO
+}
+
+void update() {
+				//TODO
+}
+
+void render() {
+				//TODO
+}
+
+void destroy_window() {
+				SDL_DestroyRenderer(renderer);
+				SDL_DestroyWindow(window);
+				SDL_Quit();
+}
 
 int main() {
-				printf("Game is running...\n");
+				game_is_running = initialize_window();
+				
+				setup();
+
+				while(game_is_running) {
+								process_input();
+								update();
+								render(); //(or draw)
+				}
+
+				destroy_window();
+
+
 				return 0;
 }
+
