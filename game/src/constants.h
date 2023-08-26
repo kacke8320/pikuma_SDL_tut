c83c0163b62f0b9981aa0bd85fc5734b88a7e20f
kacken <142664345+kacke8320@users.noreply.github.com>
2023-08-26 16:16:29 +0200

    Delete game directory

--- game/src/constants.h
+++
@@ -1,9 +0,0 @@
-#define FALSE 0
-#define TRUE 1
-
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-
-#define FPS 60
-#define FRAME_TARGET_TIME (1000 / FPS)
-				//1000/24 = 41.666 (wie lange dauert ein frame?)
